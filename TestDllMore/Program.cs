6ef5c50d36001dcdb6e25dcfcd65254489443d37
LittleMinds <LittleMinds@DESKTOP-JLBKSMO>
2016-12-07 14:29:08 +0800

    Add project files.

---
+++ TestDllMore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestDllMore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
